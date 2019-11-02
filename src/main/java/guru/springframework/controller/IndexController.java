c4d8e06620e4c69e7917e78a8d70077c9d1e850f
naetukuri <naetukuri@gmail.com>
2019-11-02 13:50:57 -0700

    SpringData Query Methods

--- src/main/java/guru/springframework/controller/IndexController.java
+++ src/main/java/guru/springframework/controller/IndexController.java
@@ -1,10 +1,29 @@
 package guru.springframework.controller;
 
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repository.CategoryRepository;
+import guru.springframework.repository.UnitOfMeasureRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+@Controller
 public class IndexController {
+    CategoryRepository categoryRepository;
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"","/","/index"})
-    String getIndexPage(){
+    public String getIndexPage(){
+        Optional<Category> categoryOptional =categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional=unitOfMeasureRepository.findByDescription("Teaspoon");
+        System.out.println("Cat Id is:"+categoryOptional.get().getId());
+        System.out.println("UPM Id is:"+unitOfMeasureOptional.get().getId());
         return "index";
     }
 }
